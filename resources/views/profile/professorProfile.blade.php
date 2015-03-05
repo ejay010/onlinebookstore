52447561d3add3b6c6a36df0b40d8944065a23d7
ejay010 <ejay010@gmail.com>
2015-03-05 06:16:34 -0500

    Added some fun stuff to look at :)

--- resources/views/profile/professorProfile.blade.php
+++ resources/views/profile/professorProfile.blade.php
@@ -1,5 +1,9 @@
 @extends('master')
 <h1>{{$user->username}}</h1>
+<img src="http://www.dodaj.rs/f/2Q/fK/1DvX60rx/geniusmixtrap.jpg">
+<br>
+Just cause she bad..
+<br>
 <a class="btn" href="professorPage/requestBook">
     <span class="glyphicon glyphicon-book"></span> Request A Book</a>
 <br>
