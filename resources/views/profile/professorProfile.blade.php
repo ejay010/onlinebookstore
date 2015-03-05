34ecae615d58b81990cfaf00ce48bbcbd3f67603
ejay010 <ejay010@gmail.com>
2015-03-05 04:35:03 -0500

    First github post,

--- resources/views/profile/professorProfile.blade.php
+++ resources/views/profile/professorProfile.blade.php
@@ -1,6 +1,6 @@
 @extends('master')
 <h1>{{$user->username}}</h1>
-<a href="professorPage/requestBook">Request A Book</a>
+<a href="professorPage/requestBook">Request A  Book</a>
 <a class="btn" href="logout">
     <span class="glyphicon glyphicon-log-out"></span> Logout</a>
 
