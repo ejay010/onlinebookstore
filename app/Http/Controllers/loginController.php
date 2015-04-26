62f7606c508ee0a81ab34205a21ed9f333fc3f9d
ejay010 <ejay010@gmail.com>
2015-04-26 15:49:46 -0400

    Add more functionality for everybody

--- app/Http/Controllers/loginController.php
+++ app/Http/Controllers/loginController.php
@@ -9,6 +9,7 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
+use Illuminate\Support\Facades\Input;
 use Illuminate\Support\Facades\Request;
 
 use App\Http\Requests\RegisterStudentRequest;
