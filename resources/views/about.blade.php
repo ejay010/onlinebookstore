ef65f77faec5255d8b829c526ed1c80f9f5ba09a
ejay010 <ejay010@gmail.com>
2015-04-27 04:59:00 -0400

    Add more functionality for everybody

---
+++ resources/views/about.blade.php
@@ -0,0 +1,31 @@
+@extends('master')
+<div class="container">
+  <div id="templatemo_header"><img src="images/newcompass.jpg" alt="" width="960" height="287" />
+
+
+    </div>
+  <!-- end of header -->
+
+    <div id="templatemo_content">
+
+        <h1 style="color: #010100"> About Us</h1>
+        <br>
+        <p style="font-size: 14px"> Tortuga Bookstore was created in 2015 as an online store. Tortuga Bookstore is the number one new source for all your textbox needs. We offer the largest selection of textbooks, novels, and entertainment available on the Web. We are commited to being number one in customer satisfaction. Don't see the book required for your class? Have your professor request it! <p>
+        <br>
+               <h1 style="color: #060606"> Contact Information</h1>
+               <br>
+               <span style="color: #0A0A09; font-weight: bold; font-size: 18px;">Email:</span><br>
+               <span style="font-style: normal; color: #030302; font-size: 18px;">TortugaBooks@Outlook.com <br><br>
+               <span style="font-weight: bold"><span style="font-size: 18px">Telephone</span>: </span><br>
+               (305) 344 9876
+               <br>
+               <br>
+               <span style="font-weight: bold">Mailing Address:</span><br>
+11567 NE Biscayne Blvd<br>
+Miami, Florida 33180<br>
+PO BOX 456<br>
+</span></span><br>
+<br>
+
+</div> <!-- end of container -->
+</div>
